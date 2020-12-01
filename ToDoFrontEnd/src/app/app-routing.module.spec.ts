14ecc06e4b048da015e21a45f37c6e7c338b2fe4
Ziyuan <zxu25@slb.com>
2020-12-01 11:51:25 +0800

    test: add test for routing module

--- ToDoFrontEnd/src/app/app-routing.module.spec.ts
+++ ToDoFrontEnd/src/app/app-routing.module.spec.ts
@@ -2,6 +2,11 @@ import { Location } from "@angular/common";
 import { TestBed, fakeAsync, tick } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { Router } from "@angular/router";
+import { routes } from "./app-routing.module"
+import { UpdateTodoItemComponent } from './update-todo-item/update-todo-item.component';
+import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
+import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
+import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
 
 describe("Router", () => {
   let location: Location;
@@ -9,8 +14,8 @@ describe("Router", () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
-      declarations: []
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [UpdateTodoItemComponent, CreateTodoitemComponent, TodoitemDetailComponent, ListTodoitemComponent]
     });
 
     router = TestBed.get(Router);
@@ -19,7 +24,9 @@ describe("Router", () => {
     router.initialNavigation();
   });
 
-  it('test navigate', fakeAsync(() =>{
-
+  it('navigate to "" redirects to /', fakeAsync(() => {
+    router.navigate(['']);
+    tick(50);
+    expect(location.path()).toBe('/');
   }))
 });
--- ToDoFrontEnd/src/app/app-routing.module.ts
+++ ToDoFrontEnd/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.compo
 import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
 import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: ListTodoitemComponent },
   { path: 'create', component: CreateTodoitemComponent },
   { path: 'edit/:id', component: UpdateTodoItemComponent },

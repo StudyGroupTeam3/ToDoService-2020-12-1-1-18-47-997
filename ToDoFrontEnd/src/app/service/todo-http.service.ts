2732012ef964f1980970a23d2c48747272f7403d
Jiankun YANG <JYang53@slb.com>
2020-12-01 15:54:40 +0800

    test: add test for create fail message

---
+++ ToDoFrontEnd/src/app/service/todo-http.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ToDoItem } from '../model/ToDoItem';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoHttpService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public GetAll(): Observable<Array<ToDoItem>>{
+    return this.httpClient.get<Array<ToDoItem>>('https://localhost:5001/ToDoItem');
+  }
+
+  public Create(todoItem: ToDoItem): Observable<ToDoItem>{
+    return this.httpClient.post<ToDoItem>('https://localhost:5001/ToDoItem',
+    todoItem, httpOptions);
+  }
+}
--- ToDoFrontEnd/src/app/service/todo.service.ts
+++ ToDoFrontEnd/src/app/service/todo.service.ts
@@ -1,50 +1,59 @@
 import { Injectable } from '@angular/core';
 import { ToDoItem } from '../model/ToDoItem';
+import { TodoHttpService } from './todo-http.service';
 import { TodoStoreService } from './todo-store.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  
+
   public updatingToDoItem: ToDoItem;
   public selectedTodoItem: ToDoItem;
   private currentId: number = 0;
 
   private _todoItems: Array<ToDoItem>;
+  public getAllFailMessage: string;
+  public createFailMessage: string;
 
-  constructor(private todoStore: TodoStoreService) {
+  constructor(private todoStore: TodoStoreService,
+    private todoHttpService: TodoHttpService) {
     this._todoItems = todoStore.GetAll();
     this.updatingToDoItem = new ToDoItem(-1, "", "", false);
     this.selectedTodoItem = new ToDoItem(-1, "", "", false);
-    this.currentId = this.todoItems.length;
+    this.getAllFailMessage = '';
+    this.createFailMessage = '';
+    //this.currentId = this.todoItems.length;
   }
 
   public get todoItems(): Array<ToDoItem> {
-    return this.todoStore.GetAll();
+   const allTodoItems = new Array<ToDoItem>();
+   this.todoHttpService.GetAll().subscribe(todoItems => {
+     allTodoItems.push(...todoItems);
+    },
+    error => this.getAllFailMessage = "get all fail because web api error");
+    return allTodoItems;
   }
 
   public SetUpdatingTodoItemId(id: number): void {
     const foundTodoItem = this.todoStore.FindById(id);
-    
+
     if (foundTodoItem !== undefined) {
       this.updatingToDoItem = Object.assign({}, foundTodoItem);
     }
   }
 
   public Create(todoItem: ToDoItem) {
-    todoItem.id = this.currentId;
-    var newTodoItem = Object.assign({}, todoItem);
-    this.todoStore.Create(newTodoItem);
-    this.currentId++;
+    this.todoHttpService.Create(todoItem).subscribe(todoItem => console.log(todoItem),
+    error => this.createFailMessage = "create fail because web api error");
   }
 
   public UpdateTodoItem(updateTodoItems: ToDoItem): void {
     this.todoStore.Update(updateTodoItems);
   }
 
-  public DeleteTodoItem(id: number):void{   
-    this.todoStore.Delete(id); 
+  public DeleteTodoItem(id: number):void{
+    this.todoStore.Delete(id);
   }
 
   public SetSelectedTodoItemId(id: number):void{

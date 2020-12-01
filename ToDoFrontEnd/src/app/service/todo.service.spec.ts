96b0f0e1ae3a2a01bb15356ddc4e82804ac75f2d
Ziyuan <zxu25@slb.com>
2020-12-01 18:16:02 +0800

    feat: add processing error message in get by id method

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -129,4 +129,18 @@ describe('TodoService', () => {
     service.SetSelectedTodoItemId(expectedTodoItem.id);
     expect(httpClientSpy.get.calls.count()).toBe(1);
   });
+
+  it('should process error response when get special todo-item fail', fakeAsync(() => {
+    // given
+    const errorResponse = new HttpErrorResponse({
+      error: 'test 404 error',
+      status: 404, statusText: 'Not Found'
+    });
+    httpClientSpy.get.and.returnValue(asyncError(errorResponse));
+    const id = 0;
+    service.SetSelectedTodoItemId(id);
+    // then
+    tick(50);
+    expect(service.getAllFailMessage).toBe('GetById fails bucause webapi error');
+  }));
 });

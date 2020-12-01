ad5a029524941154cf344c5023ba40a4ecd246bb
Ziyuan <zxu25@slb.com>
2020-12-01 13:18:48 +0800

    feat: add get all todo item in todo-http service

--- ToDoFrontEnd/src/app/service/todo-http.service.ts
+++ ToDoFrontEnd/src/app/service/todo-http.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ToDoItem } from '../model/ToDoItem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoHttpService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public GetAll(): Observable<Array<ToDoItem>> {
+    return this.httpClient.get<Array<ToDoItem>>('http://localhost:5001/ToDoItem');
+  }
 }
